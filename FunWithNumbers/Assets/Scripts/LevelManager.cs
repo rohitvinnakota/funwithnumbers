9e8674e0f608bc05882c38a46b28b835b18a9bb0
James <jsandham@uwaterloo.ca>
2018-10-13 19:01:57 -0700

    Set up basic level manager

---
+++ FunWithNumbers/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour 
+{
+	public void GoToLevel(string levelName)
+	{
+		Application.LoadLevel(levelName);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
